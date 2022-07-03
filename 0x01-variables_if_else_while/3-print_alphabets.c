37e2b53bec1c166a7569f6ad90a6543544eed004
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-03 00:38:48 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+*  Return: always returns 0
+*/
+
+int main(void)
+{
+char A;
+for (A = 'A'; A <= 'Z'; A++)
+putchar(A);
+for (A = 'A'; A <= 'Z'; A++)
+putchar(A);
+putchar('\n');
+return (0);
+}
