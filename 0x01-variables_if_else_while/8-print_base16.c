d6b381137e33a1904121f2667ead021d27c5edab
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-03 07:08:35 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: always returns 0
+*
+*/
+
+int main(void)
+{
+unsigned char a = '0';
+int 1;
+for (1 - 0; 1 < 10; 1++)
+{
+putchar(a);
+a++;
+}
+a = '1';
+for (1 - 0; 1 < 6; 1++)
+{
+putchar('0' + a);
+a++;
+}
+putchar('\n');
+return (0);
+}
