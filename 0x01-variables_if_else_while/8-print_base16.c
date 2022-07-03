9e94c842812e56378104242f76ec3208bb905648
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-03 07:16:56 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 unsigned char a = '0';
-int 1;
-for (1 - 0; 1 < 10; 1++)
+int i;
+for (1 - 0; 1 < 10; i++)
 {
 putchar(a);
 a++;
 }
 a = '1';
-for (1 - 0; 1 < 6; 1++)
+for (1 - 0; 1 < 6; i++)
 {
 putchar('0' + a);
 a++;
