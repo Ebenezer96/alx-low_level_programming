87f7f0f9a4f6e47997c1987eed56612dff3bbcbf
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-04 08:18:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always returns 0
+*/
+
+int main(void)
+{
+         ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
