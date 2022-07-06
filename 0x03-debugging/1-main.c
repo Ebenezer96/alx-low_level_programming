bf1004cd95afc721daa0e1cfde531bfe8d75b182
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-06 09:56:47 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+
+/**
+* main - cause an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *        putchar(i):
+	 */
+	printf("infinte loop avioded! \\o/\n");
+
+	return (0);
+}
