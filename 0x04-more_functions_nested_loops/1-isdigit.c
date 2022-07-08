de06f09114bbe0752c1bd9c766a4a83d97c2cc7d
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-07 19:49:38 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * _isdigit - function that verifies if a character is a digit or not
  * @c: tested character
- * Return: 1 if c is a digit, otherwise return 0.
+ * Return: returns 1 if it is uppercase, 0 if not.
  */
 int _isdigit(int c)
 {
