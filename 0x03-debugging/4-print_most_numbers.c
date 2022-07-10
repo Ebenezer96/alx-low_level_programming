1d0bb5547cc4060120c1173752de9c9d9ed95fcc
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-10 10:04:36 -0700

    4-print_most_numbers.c

--- 0x03-debugging/4-print_most_numbers.c
+++ 0x03-debugging/4-print_most_numbers.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_most_numbers - prints number from 0 to 9 but
- * execluding 2 and 4 followed by a new line 
- * Return: void
- */
+* print_most_numbers - prints number from 0 to 9 but
+* execluding 2 and 4 followed by a new line
+* Return: void
+*/
 
 void print_most_numbers(void)
 {
@@ -13,7 +13,7 @@ char c;
 
 for (c = '0'; c < '9'; c++)
 {
-if ( c != '2' && c != '4')
+if (c != '2' && c != '4')
 _putchar(c);
 }
 _putchar('\n');
