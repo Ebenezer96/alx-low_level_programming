944367e4f9a2b4e568a2ac3948201814aabee86e
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-11 09:18:52 -0700

    3-print_numbers.c

--- 0x03-debugging/3-print_numbers.c
+++ 0x03-debugging/3-print_numbers.c
@@ -1,19 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_numbers - Prints numbers from 0 to 9 followed by a new line.
- *
- * Return: 0
+ * print_numbers -> print numbers
  */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
+	for (c = 0; c < 10; c++)
+		_putchar(c + '0');
 	_putchar('\n');
 }
