8a658c490ff426498fcaa8aafddbbcae929539b1
Ebenezer96 <ebenezeramakato96@gmail.com>
2022-07-11 14:40:01 -0700

    3-prints

--- 0x03-debugging/3-print_numbers.c
+++ 0x03-debugging/3-print_numbers.c
@@ -6,9 +6,11 @@
 
 void print_numbers(void)
 {
-	int c;
+int c;
 
-	for (c = 0; c < 10; c++)
-		_putchar(c + '0');
-	_putchar('\n');
+for (c = 0; c < = 9; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
